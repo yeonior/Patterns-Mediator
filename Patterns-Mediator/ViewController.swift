17bbd3bd94b9633bce3b4b14b9c1bb238b27d6f7
Rus <de.mr44@gmail.com>
2022-02-03 00:20:16 +0500

    Initial Commit

---
+++ Patterns-Mediator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Patterns-Mediator
+//
+//  Created by Ruslan on 03.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
